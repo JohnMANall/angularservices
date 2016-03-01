af77724e8f7e3286921ab9428bb7c3930cd20abb
John M. Nall <johnmnall@yahoo.com>
2016-03-01 12:52:11 -0500

    angular basics fixes to show todo list

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -11,7 +11,7 @@ angular.module('todoListApp')
 
   dataService.getTodos(function(response) {
     console.log(response.data);
-    $scope.todos = 'response.data';
+    $scope.todos = response.data;
   });
 
   $scope.deleteTodo = function(todo, $index) {
